1acdc0ed7f6d35174922341b4b621fe53a4b41ed
Guyuang <2441484@dundee.ac.uk>
2022-01-22 21:31:34 +0800

    ceshi

---
+++ main.py
@@ -0,0 +1,14 @@
+# Standard plotly imports
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import iplot, init_notebook_mode
+# Using plotly + cufflinks in offline mode
+
+df.iplot(
+    x='read_time',	
+    y='read_ratio',	
+    # Specify the category	
+    categories='publication',	
+    xTitle='Read Time',	
+    yTitle='Reading Percent',	
+    title='Reading Percent vs Read Ratio by Publication')
